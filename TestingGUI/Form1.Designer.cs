a59f79262f890820bc986e36afafe6ba451a9c76
aboatcallie <aboatcallie@shiversoft.net>
2019-02-04 03:53:33 -0600

    initial commit

---
+++ TestingGUI/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TestingGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TestTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.VulnInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TestTimer
+            // 
+            this.TestTimer.Interval = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(460, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.VulnInfo);
+            this.groupBox1.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Silver;
+            this.groupBox1.Location = new System.Drawing.Point(12, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(476, 431);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vulnerabilities Found: 0/0";
+            // 
+            // VulnInfo
+            // 
+            this.VulnInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.VulnInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.VulnInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.VulnInfo.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VulnInfo.ForeColor = System.Drawing.Color.Silver;
+            this.VulnInfo.Location = new System.Drawing.Point(3, 19);
+            this.VulnInfo.Multiline = true;
+            this.VulnInfo.Name = "VulnInfo";
+            this.VulnInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.VulnInfo.Size = new System.Drawing.Size(470, 409);
+            this.VulnInfo.TabIndex = 0;
+            this.VulnInfo.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Silver;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "TestGUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TestTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox VulnInfo;
+    }
+}
+
---
+++ TestingGUI/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using CPVulnerabilityFramework.Core;
+using System.IO;
+using System.Xml;
+using System.Reflection;
+
+//Link dump
+// kernel32 dep policy https://docs.microsoft.com/en-us/windows/desktop/api/winbase/nf-winbase-getsystemdeppolicy
+//https://retep998.github.io/doc/kernel32/fn.GetSystemDEPPolicy.html
+//https://en.wikipedia.org/wiki/Service_Control_Manager
+
+
+namespace TestingGUI
+{
+    public partial class Form1 : Form
+    {
+        private List<Vulnerability> Vulnerabilities;
+        public Form1()
+        {
+            InitializeComponent();
+            TestTimer.Tick += TestTimer_Tick;
+            Vulnerability.AddTypeAlias("customuseralias", typeof(UA_AccountAction));
+
+            string XMLData = "";
+            XmlDocument ScoringData = new XmlDocument();
+            try
+            {
+                using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("TestingGUI.config.xml"))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    XMLData = reader.ReadToEnd();
+                }
+                ScoringData.LoadXml(XMLData);
+            }
+            catch
+            {
+                MessageBox.Show("Failed to load scoring data! XML file may be formatted incorrectly...", "Scoring Data Corrupted", MessageBoxButtons.OK);
+                Application.Exit();
+            }
+
+            Vulnerabilities = Vulnerability.ParseVulnerabilities(ScoringData);
+            foreach(var vuln in Vulnerabilities)
+            {
+                vuln.OnCompleted += VulnMessageUpdate;
+                vuln.OnRegressed += VulnMessageUpdate;
+                vuln.OnFailed += VulnMessageUpdate;
+            }
+
+            Vulnerability.LockAssembly(); //Finalize vulnerabilities from any changes
+
+            MouseDown += Form1_MouseDown1;
+            Location = new Point(0,0);
+            TestTimer.Start();
+        }
+
+        /// <summary>
+        /// Fired on vuln state changed
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="e"></param>
+        private void VulnMessageUpdate(Vulnerability caller, VulnerabilityEventArgs e)
+        {
+            //Could be optimized but might be no point
+            VulnMessages.Clear();
+            int i = 0;
+            int count = 0;
+            foreach (Vulnerability v in Vulnerabilities)
+            {
+                if (!v?.Enabled ?? true)
+                    continue;
+                if (v != 0)
+                {
+                    i++;
+                    VulnMessages.Add((int)v + " points: " + v.Message);
+                }
+                count++;
+            }
+            groupBox1.Text = "Vulnerabilities Found: " + i + "/" + count;
+
+            VulnInfo.Lines = VulnMessages.ToArray();
+        }
+
+        List<string> VulnMessages = new List<string>();
+        private void TestTimer_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "Score: " + GetScore();
+            //GC.Collect(); //keeps memory low :)
+        }
+
+        private int GetScore()
+        {
+            int result = 0;
+            foreach (Vulnerability v in Vulnerabilities)
+            {
+                result += v;
+            }
+            return result;
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
